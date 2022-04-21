984510329ed99fc745fd1b1b2d42a35d96d5a5f9
Reem <reem66am@gmail.com>
2022-04-21 23:22:35 +0300

    PHPMailer for forgot password

---
+++ ForgotPassword.php
@@ -0,0 +1,45 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$email = $_POST['email'];
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'PetHouse.coo@gmail.com';                     //SMTP username
+    $mail->Password   = 'Pethouse123';                               //SMTP password
+    $mail->SMTPSecure = 'tls';           //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('PetHouse.coo@gmail.com', 'PetHouse');
+    $mail->addAddress($email);     //Add a recipient
+    
+    
+
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'OTP';
+    $mail->Body    = 'Here is your secure code 4377 <br><br> !PetHouse Glad That You Are Part of It';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
