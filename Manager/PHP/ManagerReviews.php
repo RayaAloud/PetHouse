db833dde2d06ccb33d74df25f0078dab9c8769a2
Reem <reem66am@gmail.com>
2022-05-18 17:50:48 +0300

    Stars in Reviews

--- Manager/PHP/ManagerReviews.php
+++ Manager/PHP/ManagerReviews.php
@@ -17,17 +17,17 @@ while ($data = mysqli_fetch_array($result)) {
    <h5 class='card-title pb-2'><i class='fa-solid fa-user' style='padding-right: 10px;'></i>".$owner_name['First_Name']."</h5>");
   if($data['Rate'] == 0){
     print("<div class='pb-2'>
-            <i class='fa-solid fa-star' style='color: gray;'></i><i class='fa-solid fa-star' style='color: gray;'></i><i class='fa-solid fa-star' style='color: gray;'></i><i class='fa-solid fa-star' style='color: gray;''></i><i class='fa-solid fa-star' style='color: gray;'></i>
+            <i class='fa-solid fa-star' style='color: rgb(173, 173, 173);'></i><i class='fa-solid fa-star' style='color: rgb(173, 173, 173);'></i><i class='fa-solid fa-star' style='color: rgb(173, 173, 173);'></i><i class='fa-solid fa-star' style='color: rgb(173, 173, 173);'></i><i class='fa-solid fa-star' style='color: rgb(173, 173, 173);'></i>
           </div>");
   }
   else if($data['Rate'] == 1){
     print("<div class='pb-2'>
-            <i class='fa-solid fa-star'></i><i class='fa-solid fa-star' style='color: gray;'></i><i class='fa-solid fa-star' style='color: gray;'></i><i class='fa-solid fa-star' style='color: gray;'></i><i class='fa-solid fa-star' style='color: gray;'></i>
+            <i class='fa-solid fa-star' style='color: rgb(247, 183, 65)'></i><i class='fa-solid fa-star' style='color: rgb(173, 173, 173);'></i><i class='fa-solid fa-star' style='color: rgb(173, 173, 173);'></i><i class='fa-solid fa-star' style='color: rgb(173, 173, 173);'></i><i class='fa-solid fa-star' style='color: rgb(173, 173, 173);'></i>
           </div>");
   }
   else if($data['Rate'] == 2){
     print("<div>
-            <i class='fa-solid fa-star'></i><i class='fa-solid fa-star'></i><i class='fa-solid fa-star' style='color: gray;'></i><i class='fa-solid fa-star' style='color: gray;'></i><i class='fa-solid fa-star' style='color: gray;'></i>
+            <i class='fa-solid fa-star' style='color: rgb(247, 183, 65)'></i><i class='fa-solid fa-star' style='color: rgb(247, 183, 65)'></i><i class='fa-solid fa-star' style='color: rgb(173, 173, 173);'></i><i class='fa-solid fa-star' style='color: rgb(173, 173, 173);'></i><i class='fa-solid fa-star' style='color: rgb(173, 173, 173);'></i>
           </div>");
   }
   else if($data['Rate'] == 3){
@@ -37,12 +37,12 @@ while ($data = mysqli_fetch_array($result)) {
   }
   else if($data['Rate'] == 4){
     print("<div>
-    <i class='fa-solid fa-star'></i><i class='fa-solid fa-star'></i><i class='fa-solid fa-star'></i><i class='fa-solid fa-star' style='color: rgb(223, 170, 44);'></i><i class='fa-solid fa-star' style='color: gray;'></i>
+    <i class='fa-solid fa-star' style='color: rgb(247, 183, 65)'></i><i class='fa-solid fa-star' style='color: rgb(247, 183, 65)'></i><i class='fa-solid fa-star' style='color: rgb(247, 183, 65)'></i><i class='fa-solid fa-star' style='color: rgb(247, 183, 65)'></i><i class='fa-solid fa-star' style='color: rgb(173, 173, 173);'></i>
      </div>");
   }
   else if($data['Rate'] == 5){
     print("<div>
-    <i class='fa-solid fa-star'></i><i class='fa-solid fa-star'></i><i class='fa-solid fa-star'></i><i class='fa-solid fa-star'></i><i class='fa-solid fa-star'></i>
+    <i class='fa-solid fa-star' style='color: rgb(247, 183, 65)'></i><i class='fa-solid fa-star' style='color: rgb(247, 183, 65)'></i><i class='fa-solid fa-star' style='color: rgb(247, 183, 65)'></i><i class='fa-solid fa-star' style='color: rgb(247, 183, 65)'></i><i class='fa-solid fa-star' style='color: rgb(247, 183, 65)'></i>
      </div>");
   }
  print("<p class='card-text'>".$data['Opinion']."</p>"."<p class='contact_owner'><i class='bi bi-envelope-fill emailIcon'></i><a href='.'mailto:'".$data['Owner_Email']."'> Contact Pet Owner</a></p></div></div> ");
