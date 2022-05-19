93048915ec2686d08e6efe9051238944c5175060
Reem <reem66am@gmail.com>
2022-05-19 15:54:18 +0300

    upcoming dashboard ✅ Except Note position

--- Pet_Owner/PHP/UpcomingDashboard.php
+++ Pet_Owner/PHP/UpcomingDashboard.php
@@ -10,24 +10,42 @@ $result=mysqli_query($connection, $query);
 
 if ($result) {
  while ($appt = mysqli_fetch_array($result)) {
-
-  $date = explode("-",$appt['Date']);
-  if($date > date("Y-m-d")){
-    $date = $date[2]."/".$date[1]."/".$date[0];
      $pet = mysqli_query($connection,"SELECT * FROM Pet WHERE ID =".$appt['PetID'].";");
      $pet = mysqli_fetch_array($pet);
      print("
      <tr id='".$appt['Request_ID']."'>      
      <td><img class='t-img' src='data:image/png;charset=utf8;base64,".base64_encode($pet['Photo'])."' alt='Pet Photo'><span class='fs-5'>&nbsp;&nbsp;".$pet['Name']."</span></td>
      <td>".$appt['Service_Name']."</td>
-     <td>".$date."</td>
-     <td>".$appt['Time']."</td>
+     <td>".dateFormat($appt['Date'])."</td>
+     <td>".timeFormat($appt['Time'])."</td>
      <td><button onclick='showNote(this)'> <i class='bi bi-chat-square-dots-fill noteIcon'></i> </button></td>
      <tr>"
      );
     }
-  }  
+   
 } 
-else
-  echo "An error occured.";     
+function timeFormat($time){
+  $time = explode(":",$time);
+  if($time[0] < 12){
+    if($time[0] == 0)
+      $time = ($time[0]+12).":".$time[1]." AM";
+    else{
+      $time = $time[0].":".$time[1]." AM";
+    }
+  }
+  else{
+      if($time[0] == 12){
+          $time = $time[0].":".$time[1]." PM";
+      }
+      else{
+          $time = ($time[0]-12).":".$time[1]." PM";
+      }
+  }
+  return $time;
+}
+function dateFormat($date){
+  $date = explode("-",$date);
+  $date = $date[2]."/".$date[1]."/".$date[0];
+  return $date;
+}     
 ?>
